6dfd1a559586dc22132d2b2acc94390eb0c3b5df
Wojciech Skrzypiec <wojciech.skrzypiec2@gmail.com>
2026-03-30 13:03:06 +0200

    Plotting improvements

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 
-INSTANCE_ROOT = Path("..").resolve() / "instances"
-INSTANCE_SELECTED_PATH = INSTANCE_ROOT / \
-    "SelectedInstances" / "selected_instances_data.csv"
+ROOT = Path(__file__).resolve().parent.parent
+INSTANCE_ROOT = ROOT / "instances"
 INSTANCE_FJSSPW_PATH = INSTANCE_ROOT / "Example_Instances_FJSSP-WF"
 INSTANCE_DATA_ROOT = INSTANCE_ROOT / "InstanceData"
 INSTANCE_DATA_FJSSPW_PATH = INSTANCE_DATA_ROOT / "FJSSP-W"
+INSTANCE_DATA_SELECTED_PATH = INSTANCE_ROOT / \
+    "SelectedInstanceData" / "selected_instances_data.csv"
